ace5ff5df03b08f39233683810392629e88bd1b0
Jacopo <beschi.jacopo@gmail.com>
2014-03-13 10:21:04 +0100

    fix img prod val

--- src/Palmabit/Catalog/Validators/ProductImageValidator.php
+++ src/Palmabit/Catalog/Validators/ProductImageValidator.php
@@ -5,7 +5,7 @@ use Palmabit\Library\Validators\OverrideConnectionValidator;
 class ProductImageValidator extends OverrideConnectionValidator
 {
     protected static $rules = array(
-        "description" => "max:255",
+        "description" => "max:255|required",
         'image' => ['required','image','min:1', 'max:4096']
     );
 }
