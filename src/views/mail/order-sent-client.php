603d6c307cc692cf85be080ffc1920ab01ea12a5
Francesco <f.falanga@palmabit.com>
2014-03-18 22:35:24 +0100

    email translation for catalog

--- src/views/mail/order-sent-client.php
+++ src/views/mail/order-sent-client.php
@@ -5,21 +5,21 @@
     <meta charset="utf-8">
 </head>
 <body>
-<h2>Ordine su {{Config::get('authentication::app_name')}} eseguito con successo</h2>
+<h2>{{Config::get('authentication::app_name')}}: {{L::t('Order submitted successfully')}}</h2>
 <div>
     Buongiorno {{ $body['email'] }}
-    <strong>L'ordine numero: {{$body['order']->id}} è stato inoltrato con successo.</strong>
+    <strong>{{L::t('Order number')}}: {{$body['order']->id}} {{L::t('submitted successfully')}}.</strong>
     <br/>
-    <strong>Dettagli ordine:</strong>
+    <strong>{{L::t('Order details')}}:</strong>
     <ul>
         @foreach($body['order']->row_order()->get() as $order)
             <? $product = Product::find($order->product_id); ?>
             <li>
-                <strong>nome: </strong>{{$product->name}}
+                <strong>{{L::t('Name')}}: </strong>{{$product->name}}
             </li>
         @endforeach
     </ul>
-    <a href="{{URL::to('/')}}" target="_blank">Vai al sito</a>
+    <a href="{{URL::to('/')}}" target="_blank">Homepage {{Config::get('authentication::app_name')}}</a>
 </div>
 </body>
 </html>
