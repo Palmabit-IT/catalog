5661088fa80442224dcc537d0f1cae1506fcb6f5
Jacopo <beschi.jacopo@gmail.com>
2014-03-20 10:22:19 +0100

    align order 3

---
+++ src/Palmabit/Catalog/Presenters/Interfaces/ProductCategoryPresenterInterface.php
@@ -0,0 +1,13 @@
+<?php  namespace Palmabit\Catalog\Presenters\Interfaces; 
+/**
+ * Interface ProductCategoryPresenterInterface
+ *
+ * @author jacopo beschi j.beschi@palmabit.com
+ */
+interface ProductCategoryPresenterInterface 
+{
+    public function featured_image();
+    public function description();
+    public function name();
+    public function getLink();
+}
